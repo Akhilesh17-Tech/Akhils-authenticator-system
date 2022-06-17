74b940be33bebfe7171369c018f818b6f5f96767
Akhilesh kushwah <Akhileshkushwah17@gmail.com>
2022-06-17 18:13:58 +0530

    changes made

--- workers/email_worker.js
+++ workers/email_worker.js
@@ -1,6 +1,6 @@
 const queue = require("../config/kue");
 const resetPasswordMailer = require("../mailers/forgot_password_mailer");
-console.log("============ akhilesh");
+// console.log("============ akhilesh");
 queue.process("emails", function (job, done) {
   console.log("worker is processing the jobs", job.data);
   resetPasswordMailer.newResetLink(job.data);
